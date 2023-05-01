65f22fe1e7d4066335078c75de198ec70a2b191f
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-05-01 19:09:38 +0530

    Add files via upload

---
+++ VirtualBaseClass.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+class Student {
+protected:
+  int roll_no;
+
+public:
+  void set_roll(int a) { roll_no = a; }
+  void print_roll(void) { cout << "Your roll no is " << roll_no << endl; }
+};
+
+class Test : virtual public Student {
+protected:
+  float maths, physics;
+
+public:
+  void set_marks(float m1, float m2) {
+    maths = m1;
+    physics = m2;
+  }
+
+  void print_marks(void) {
+    cout << "Your result is here: " << endl
+         << "Maths: " << maths << endl
+         << "Physics: " << physics << endl;
+  }
+};
+
+class Sports : public virtual Student {
+protected:
+  float score;
+
+public:
+  void set_score(float sc) { score = sc; }
+
+  void print_score(void) { cout << "Your PT score is " << score << endl; }
+};
+
+class Result : public Test, public Sports {
+private:
+  float total;
+
+public:
+  void display(void) {
+    total = maths + physics + score;
+    print_roll();
+    print_marks();
+    print_score();
+    cout << "Your total score is: " << total << endl;
+  }
+};
+int main() {
+  Result rumi;
+  rumi.set_roll(155);
+  rumi.set_marks(90.1, 89.5);
+  rumi.set_score(8);
+  rumi.display();
+}
