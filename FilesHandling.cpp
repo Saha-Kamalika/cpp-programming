e91a7a64630ecae8b5372d0e75ac090130928c9b
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-05-05 08:32:24 +0530

    Add files via upload

---
+++ FilesHandling.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+/*
+  The classes for working with files in c++ are:
+  1. fstreambase
+  2. ofstream  -->derived from fstreambase
+  3. ifstream   -->derived from fstreambase
+
+  Files can be opened in 2 ways:
+  1. using the constrcutor
+  2. using the member function open() of the class
+*/
+int main() {
+  // Writing to a file
+  string s1 = "Hi this is my output file";
+  string s2;
+  ofstream out("Sample1.txt");
+  out << s1;
+
+  // Reading from a file
+  ifstream in("Sample2.txt");
+  //in >> s2; // gets a single word
+  //cout << s2;
+  getline(in, s2);
+  cout << s2;
+
+  return 0;
+}
