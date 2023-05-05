a01dd59d9d7b03a58f8bdbca2454175b4cbaca77
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-05-05 08:56:18 +0530

    Update FilesHandling.cpp

--- FilesHandling.cpp
+++ FilesHandling.cpp
@@ -12,18 +12,32 @@ using namespace std;
   2. using the member function open() of the class
 */
 int main() {
-  // Writing to a file
+  // Writing to a file using constructor
   string s1 = "Hi this is my output file";
   string s2;
   ofstream out("Sample1.txt");
   out << s1;
 
+  // Writing to a file using open()
+  string s3 = "Hi this is my output file";
+  ofstream oout;
+  oout.open("Sample1.txt");
+  oout<<s3;
+  oout.close(); //mandatory
+
   // Reading from a file
   ifstream in("Sample2.txt");
   //in >> s2; // gets a single word
   //cout << s2;
-  getline(in, s2);
-  cout << s2;
+  //getline(in, s2);
+  //cout << s2;
 
+  //Reading from a file using eof()-->end of line
+  while(in.eof()==0){
+    getline(in,s2);
+    cout<<s2<<endl;
+  }
+  in.close();
+  
   return 0;
 }
