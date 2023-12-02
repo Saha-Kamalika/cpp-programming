4ab34a511eb2ba9c99dccfc752ec87d9162ff245
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-12-02 08:48:58 +0530

    Create inorderSuccessor.cpp

---
+++ DSA/Trees/inorderSuccessor.cpp
@@ -0,0 +1,21 @@
+class Solution{
+  public:
+    Node * search(Node *ptr, Node *x){
+        if (ptr==NULL)
+            return NULL;
+        if (ptr->data > x->data){
+            Node *left=search(ptr->left,x);
+            if (left!=NULL)
+                return left;
+            return ptr;
+        }    
+        return search(ptr->right,x);
+    }
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        Node *res=search(root,x);
+        if(res!=NULL)
+            return res;
+        return new Node(-1);    
+    }
+};
