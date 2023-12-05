1ad4bd6cf29030622d49e04a080a95bcd04780c4
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-12-05 09:59:54 +0530

    Create k closest elements.cpp

---
+++ DSA/Heaps/k closest elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>> maxh;
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++){
+            maxh.push({abs(arr[i]-x),arr[i]});
+            if(maxh.size()>k) maxh.pop();
+        }
+        while(maxh.size()>0){
+            ans.push_back(maxh.top().second);
+            maxh.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
