060a79449193bc46d912f5ad1540611597d06a45
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-12-07 16:42:13 +0530

    Create min cost to connect n ropes.cpp

---
+++ DSA/Heaps/min cost to connect n ropes.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        priority_queue<int,vector<int>,greater<int>> minh;
+        for(int i=0;i<n;i++) minh.push(arr[i]);
+        long long cost=0;
+        while(minh.size()>=2){
+            long f=minh.top();
+            minh.pop();
+            long s=minh.top();
+            minh.pop();
+            cost+=f+s;
+            minh.push(f+s);
+        }
+        return cost;
+    }
+};
