15bd9d8586b0673d0c0306b844a368c480b4a259
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2024-03-06 14:42:19 +0530

    Add files via upload

---
+++ DSA/Trees/modifiedBFS.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node {
+public:
+  int data;
+  Node *left;
+  Node *right;
+  Node(int data) {
+    this->data = data;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+Node *buildTree(Node *root) {
+  cout << "Enter the data: " << endl;
+  int data;
+  cin >> data;
+  root = new Node(data);
+  if (data == -1)
+    return NULL;
+  cout << "Enter data for inserting in left of " << data << endl;
+  root->left = buildTree(root->left);
+  cout << "Enter data for inserting in right of " << data << endl;
+  root->right = buildTree(root->right);
+  return root;
+}
+void levelOrder(Node *root) {
+  queue<Node *> q;
+  q.push(root);
+  q.push(NULL);
+
+  while (!q.empty()) {
+    Node *temp = q.front();
+    q.pop();
+
+    if (temp == NULL) {
+      cout << endl;
+      if (!q.empty()) {
+        q.push(NULL);
+      }
+    } else {
+      cout << temp->data << " ";
+      if (temp->left) {
+        q.push(temp->left);
+      }
+      if (temp->right) {
+        q.push(temp->right);
+      }
+    }
+  }
+}
+int main() {
+  Node *root = NULL;
+  root = buildTree(root);
+  levelOrder(root);
+}
